4561f98a0790aa1327ab89ccee25ef588a55ca92
Klaas Schoute <klaas_schoute@hotmail.com>
2023-08-14 09:38:26 +0200

    Update code for Ruff

--- src/muenster/muenster.py
+++ src/muenster/muenster.py
@@ -6,7 +6,7 @@ import json
 import socket
 from dataclasses import dataclass
 from importlib import metadata
-from typing import Any, cast
+from typing import TYPE_CHECKING, Any, cast
 
 import async_timeout
 from aiohttp import ClientError, ClientSession
@@ -15,6 +15,9 @@ from yarl import URL
 
 from .exceptions import ODPMuensterConnectionError, ODPMuensterError
 from .models import Garage
+
+if TYPE_CHECKING:
+    from typing_extensions import Self
 
 
 @dataclass
@@ -105,7 +108,7 @@ class ODPMuenster:
         if self.session and self._close_session:
             await self.session.close()
 
-    async def __aenter__(self) -> ODPMuenster:
+    async def __aenter__(self) -> Self:
         """Async enter.
 
         Returns
@@ -114,7 +117,7 @@ class ODPMuenster:
         """
         return self
 
-    async def __aexit__(self, *_exc_info: Any) -> None:
+    async def __aexit__(self, *_exc_info: object) -> None:
         """Async exit.
 
         Args:
@@ -223,7 +226,7 @@ class StadtMuenster:
         if self.session and self._close_session:
             await self.session.close()
 
-    async def __aenter__(self) -> StadtMuenster:
+    async def __aenter__(self) -> Self:
         """Async enter.
 
         Returns
@@ -232,7 +235,7 @@ class StadtMuenster:
         """
         return self
 
-    async def __aexit__(self, *_exc_info: Any) -> None:
+    async def __aexit__(self, *_exc_info: object) -> None:
         """Async exit.
 
         Args:
